fe712acab6f3447305e204b17940b5e07ea044a3
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-06-09 14:48:23 -0600

    added routes/workout files

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const workoutRoutes = require("./routes/workouts");
 
 // express app
 const app = express();
@@ -10,6 +11,9 @@ app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
+
+// routes
+app.use(workoutRoutes);
 
 app.get("/", (req, res) => {
   res.json({ mssg: "Welcome to the app" });
