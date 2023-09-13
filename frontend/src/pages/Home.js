e530f987f47c6ed72663e6f9d1cd0bbd5d5fa933
Matthew Jackson <120997192+mattyj7@users.noreply.github.com>
2023-09-13 14:01:01 -0600

    Modified fetch data function

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,17 @@
 import { useEffect, useState } from 'react'
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
 
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch('http://localhost:4000/api/workouts')
+      const json = await response.json()
+      
+      if (response.ok) {
+        setWorkouts(json)
+      }
+    
     }
 
     fetchWorkouts()
@@ -12,7 +19,11 @@ const Home = () => {
 
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts && workouts.map((workout) => (
+          <p key={workout._id}>{workout.title}</p>
+        ))}
+      </div>
     </div>
   );
 };
