34784192b82cfaff97c0bb0f840d5f4eba5be8b6
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-09-16 13:29:11 -0600

    added WorkoutContext for local use

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -2,6 +2,31 @@ import { createContext } from "react";
 
 export const WorkoutContext = createContext();
 
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [action.payload, ...state.workouts],
+      };
+  }
+};
+
+export const WorkoutsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, {
+    workouts: null,
+  });
+
+  dispatch({ type: "SET_WORKOUTS", payload: [{}, {}] });
+};
+
 const WorkoutsContextProvider = ({ children }) => {
-  return <WorkoutContext.Provider>{children}</WorkoutContext.Provider>;
+  return (
+    <WorkoutContext.Provider value={{ state, dispatch }}>
+      {children}
+    </WorkoutContext.Provider>
+  );
 };
