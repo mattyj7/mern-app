68ecdc0b7d9664bcd6d4c2237964b2176c17a5b8
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-09-17 09:09:19 -0600

    Added Delete Context

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,10 +1,26 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
       <p>Load (kg): {workout.load}</p>
       <p>Reps: {workout.reps}</p>
       <p>{workout.CreatedAt}</p>
+      <span onClick={handleClick}>Delete</span>
     </div>
   );
 };
--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -8,9 +8,13 @@ export const workoutsReducer = (state, action) => {
       return {
         workouts: action.payload,
       };
-    case "CREATE_WORKOUT":
+    case "CREATE_WORKOUTS":
       return {
         workouts: [action.payload, ...state.workouts],
+      };
+    case "DELETE_WORKOUT":
+      return {
+        workouts: state.workouts.filter((w) => w._id !== action.payload._id),
       };
     default:
       return state;
